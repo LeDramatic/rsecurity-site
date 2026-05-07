eb147d157200820d7bd26662cf4f68c996c2669b
LeDramatic <ionas8936@gmail.com>
2026-05-07 23:30:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const API_BASE_URL = "https://rsecurity-backend.onrender.com";
+
+function getIP() {
+  fetch(`${API_BASE_URL}/api/ip`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("ip").innerText = data.ip;
+    })
+    .catch(() => {
+      document.getElementById("ip").innerText = "Unavailable";
+    });
+}
