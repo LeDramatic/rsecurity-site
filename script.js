ee06401720cd025fc5e0726f6218a07d84e2b07c
LeDramatic <ionas8936@gmail.com>
2026-05-08 20:17:00 +0300

    fix script merge conflict

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const API_BASE_URL = "https://rsecurity-backend.onrender.com";
 
 function getIP() {
@@ -11,6 +10,7 @@ function getIP() {
       document.getElementById("ip").innerText = "Unavailable";
     });
 }
+
 fetch(`${API_BASE_URL}/api/visit`, {
   method: "POST",
   headers: {
@@ -20,17 +20,3 @@ fetch(`${API_BASE_URL}/api/visit`, {
     page: window.location.pathname
   })
 });
-=======
-const API_BASE_URL = "https://rsecurity-backend.onrender.com";
-
-function getIP() {
-  fetch(`${API_BASE_URL}/api/ip`)
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("ip").innerText = data.ip;
-    })
-    .catch(() => {
-      document.getElementById("ip").innerText = "Unavailable";
-    });
-}
->>>>>>> fff7b7be55273fe5c6dca200e924846e36a8f6af
