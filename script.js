5f7dcc34113566c692405e6955639e3ad9f658c5
LeDramatic <ionas8936@gmail.com>
2026-05-08 19:57:32 +0300

    frontend setup

---
+++ script.js
@@ -0,0 +1,21 @@
+const API_BASE_URL = "https://rsecurity-backend.onrender.com";
+
+function getIP() {
+  fetch(`${API_BASE_URL}/api/ip`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("ip").innerText = data.ip;
+    })
+    .catch(() => {
+      document.getElementById("ip").innerText = "Unavailable";
+    });
+}
+fetch(`${API_BASE_URL}/api/visit`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    page: window.location.pathname
+  })
+});
